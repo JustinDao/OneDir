d144ee0b978143c1cfeea596fd0609edd7f6df9d
Justin Dao <tjd5at@virginia.edu>
2014-03-23 17:00:01 -0400

    Sets up sqlite3 DB, adds user login/sign up

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import request
+import sqlite3
+
+
 
 @app.route('/')
 @app.route('/index')
@@ -9,13 +12,61 @@ def index():
 @app.route('/login', methods=['POST'])
 def login():
   if valid_login(request.form['username'], request.form['password']):
-    return "Valid!"
+    return "valid"
   else:
-    return "Not Valid :("
-   
+    return "invalid"
+
+@app.route('/check_username', methods=['POST'])
+def check_username_request():
+  if user_exists(request.form['username']):
+    return "invalid"
+  else:
+    return "valid"
+
+@app.route('/create_user', methods=['POST'])
+def create_user_request():
+  create_user(request.form['username'], request.form['password']) 
+  return "User created"
 
 def valid_login(username, password):
-  if username == "tjd5at" and password == "hunter2":
+  connection = sqlite3.connect('server.db')
+  cursor = connection.cursor()
+
+  info = (username, password)
+  cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", info)
+
+  user = cursor.fetchall()
+
+  connection.commit()
+  connection.close()
+
+  if user:
     return True
   else:
     return False
+
+def user_exists(username):
+  connection = sqlite3.connect('server.db')
+  cursor = connection.cursor()
+
+  cursor.execute("SELECT * FROM users WHERE username = ?", (username,) )
+
+  user = cursor.fetchall()
+
+  connection.commit()
+  connection.close()
+
+  if user:
+    return True
+  else:
+    return False
+
+def create_user(username, password):
+  connection = sqlite3.connect('server.db')
+  cursor = connection.cursor()
+
+  info = (username, password)
+  cursor.execute("INSERT INTO users VALUES(?, ?)", info)
+
+  connection.commit()
+  connection.close()
--- watchdog_example.py
+++ watchdog_example.py
@@ -26,39 +26,69 @@ class OneDirHandler(FileSystemEventHandler):
 def get_username():
     return pwd.getpwuid(os.getuid()).pw_name
 
+def start_service():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    username = get_username()
+    directory = "/home/" + username + "/onedir/"
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    event_handler = OneDirHandler()
+    # logging_handler = LoggingEventHandler()
+    
+    observer = Observer()
+    # observer.schedule(logging_handler, directory, recursive=True)
+    observer.schedule(event_handler, directory, recursive=True)
+
+    observer.start()
+    print "Service started."
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
 if __name__ == "__main__":
-    print "Login:"
-    username = raw_input("username: ")
-    password = raw_input("password: ")
+    print "Enter 'login' to login, or 'sign up' to create a new account:"
 
-    login_info = {'username': username, 'password': password}
-    r = requests.post(server_url+"/login", data=login_info)
-    if (r.text == "Valid!"):
-        print "Valid Login!"
-        logging.basicConfig(level=logging.INFO,
-                            format='%(asctime)s - %(message)s',
-                            datefmt='%Y-%m-%d %H:%M:%S')
+    valid_inputs = ["login", "signup", "sign up"]
 
-        username = get_username()
-        directory = "/home/" + username + "/onedir/"
+    command = raw_input("")
+    while(command.lower() not in valid_inputs):
+        command = raw_input("")
 
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+    if command.lower() == "login":
+        print "Login:"
+        username = raw_input("username: ")
+        password = raw_input("password: ")
 
-        event_handler = OneDirHandler()
-        logging_handler = LoggingEventHandler()
-        
-        observer = Observer()
-        # observer.schedule(logging_handler, directory, recursive=True)
-        observer.schedule(event_handler, directory, recursive=True)
+        login_info = {'username': username, 'password': password}
+        r = requests.post(server_url+"/login", data=login_info)
+        if (r.text == "valid"):
+            print "Valid Login!"
+            start_service()
+        else:
+            print "Invalid login!"
+    elif command.lower() == "sign up" or command.lower() == "signup":
+        print "Sign up for OneDir!"
 
-        observer.start()
-        print "Service started."
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            observer.stop()
-        observer.join()
-    else:
-        print "Invalid login!"
+        username = raw_input("Enter a username: ")
+        username_info = {"username": username}
+        username_request = requests.post(server_url+"/check_username", data=username_info)
+
+        while username_request.text == "invalid":
+            username = raw_input("Username already exists. Enter another username: ")
+            username_info = {"username": username}
+            username_request = requests.post(server_url+"/check_username", data=username_info)
+        password = raw_input("Enter a password: ")
+
+        create_user_info = {'username': username, 'password': password}
+
+        create_user_request = requests.post(server_url+"/create_user", data=create_user_info)
+
+        start_service()
