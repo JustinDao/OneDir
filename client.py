61a0fdd2eb2330eac503b9bebaeb0749d6d0f383
Justin Dao <tjd5at@virginia.edu>
2014-03-25 12:06:04 -0400

    Creates file when file is moved from outside watched folder to inside

--- client.py
+++ client.py
@@ -7,14 +7,19 @@ import os
 import pwd
 import requests
 import getpass
+import collections
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 from watchdog.events import FileSystemEventHandler
+from watchdog.events import FileCreatedEvent
+from datetime import datetime
 
 server_url = "http://localhost:5000"
 
 username = ""
 password = ""
+
+updated_at = str(datetime.now())
 
 def get_username():
     return pwd.getpwuid(os.getuid()).pw_name
@@ -23,6 +28,10 @@ main_username = get_username()
 directory = "/home/" + main_username + "/onedir/"
 
 class OneDirHandler(FileSystemEventHandler):
+    def on_any_event(self, event):
+        global updated_at
+        updated_at = str(datetime.now())
+
     def on_created(self, event):
         print "Created " + event.src_path
 
@@ -66,18 +75,70 @@ class OneDirHandler(FileSystemEventHandler):
         # print r.text
 
     def on_moved(self, event):
-        print "Moved " + event.src_path + " to " + event.dest_path
+        if event.src_path is not None:
+            print "Moved " + event.src_path + " to " + event.dest_path
 
-        #handles file vs. dir on server
-        
-        src = event.src_path.replace(directory, "")
-        dest = event.dest_path.replace(directory, "")
+            #handles file vs. dir on server
+            
+            src = event.src_path.replace(directory, "")
+            dest = event.dest_path.replace(directory, "")
 
-        info = {'username': username, 'password': password, 'src': src, 'dest': dest}
+            info = {'username': username, 'password': password, 'src': src, 'dest': dest}
 
-        r = requests.put(server_url +"/move_item", data=info)
+            r = requests.put(server_url +"/move_item", data=info)
 
-        # print r.text
+            # print r.text
+        else:
+            new_event = FileCreatedEvent(event.dest_path)
+            self.on_created(new_event)
+
+def unicode_dict_to_string(data):
+    '''
+    http://stackoverflow.com/questions/1254454/
+    '''
+    if isinstance(data, basestring):
+        return str(data)
+    elif isinstance(data, collections.Mapping):
+        return dict(map(unicode_dict_to_string, data.iteritems()))
+    elif isinstance(data, collections.Iterable):
+        return type(data)(map(unicode_dict_to_string, data))
+    else:
+        return data
+
+def get_file_list(path):
+    """
+    http://code.activestate.com/recipes/577879-create-a-nested-dictionary-from-oswalk/
+    Creates a nested dictionary that represents the folder structure of rootdir
+    """
+
+    dir = {}
+    rootdir = path.rstrip(os.sep)
+    start = rootdir.rfind(os.sep) + 1
+    for path, dirs, files in os.walk(rootdir):
+        folders = path[start:].split(os.sep)
+        subdir = dict.fromkeys(files)
+        parent = reduce(dict.get, folders[:-1], dir)
+        parent[folders[-1]] = subdir
+    return dir
+
+
+
+def check_files(files):
+    un = username.decode('unicode-escape')
+    dir = get_file_list(directory)
+    if dir['onedir'] != files[username]:
+        diff = [x for x in dir['onedir'].keys() if x not in files[username].keys()]
+        diff += [x for x in files[username].keys() if x not in dir['onedir'].keys()]
+        print diff
+        # r = requests.get(server_url +"/get_file", data=info)
+
+def request_files():
+    info = {'username': username, 'password': password}
+    r = requests.get(server_url +"/request_files", data=info)
+    files =  r.json()
+    files = unicode_dict_to_string(files)
+    check_files(files)
+
 
 
 def start_service():
@@ -100,6 +161,7 @@ def start_service():
     try:
         while True:
             time.sleep(1)
+            request_files()
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
